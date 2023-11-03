1003241499c1600ed76ac8fb3e9708befccfab1d
MohamedGoush <0x1110111@gmail.com>
2023-11-03 21:32:42 +0530

    Initial Commit

---
+++ NavigationStackAPI/NavigationStackAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationStackAPIApp.swift
+//  NavigationStackAPI
+//
+//  Created by python on 03/11/23.
+//
+
+import SwiftUI
+
+@main
+struct NavigationStackAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
